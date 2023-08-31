17318e7e971b1cdb346e42fd273d5146c057d52c
RyanMurphy1014 <ryan.murphy611@gmail.com>
2023-08-31 14:02:04 -0400

    Clean up of unused DbInteractor functions

--- interactors/dbInteractor.js
+++ interactors/dbInteractor.js
@@ -6,7 +6,11 @@ const databaseImplementationOrganizations = require("../infra/db/mockOrganizatio
 
 //Credentials
 function login(organizationName, username, password) {
-  const attemptedLogin = databaseImplementationCredentials.login(organizationName, username, password);
+  const attemptedLogin = databaseImplementationCredentials.login(
+    organizationName,
+    username,
+    password
+  );
   if (attemptedLogin != null) {
     return attemptedLogin;
   } else {
@@ -14,70 +18,8 @@ function login(organizationName, username, password) {
   }
 }
 
-function createUserCredentials(username, password) {
-  if (databaseImplementationCredentials.createUserCredentials(username, password) === 1) {
-    return 1;
-  } else {
-    console.log("Unable to create user.");
-  }
-}
-
-//Users
-/**
- * @returns User object with populated info from database
- * @param {number} id - Id of user to lookup in database
- */
-function getUserData(organizationName, id) {
-  return databaseImplementationOrganizations.findUserById(organizationName, id);
-}
-
-/**
- *
- * @param {String} valueBeingChanged Must be specifc option defined inside this method
- * @param {String} newValue
- * @param {String} editedUserId
- */
-function setUserValue(valueBeingChanged, newValue, editedUserId) {
-  if (valueBeingChanged === "name") {
-    getUserData(editedUserId).name = newValue;
-    console.log("Change Successful!");
-  }
-  if (valueBeingChanged === "email") {
-    getUserData(editedUserId).email = newValue;
-    console.log("Change Successful!");
-  }
-  if (valueBeingChanged === "role") {
-    getUserData(editedUserId).role = newValue;
-    console.log("Change Successful!");
-  }
-  if (valueBeingChanged === "organizations") {
-    getUserData(editedUserId).organizations = newValue;
-    console.log("Change Successful!");
-  }
-  if (valueBeingChanged === "projects") {
-    getUserData(editedUserId).projects = newValue;
-    console.log("Change Successful!");
-  }
-  if (valueBeingChanged === "teams") {
-    getUserData(editedUserId).teams = newValue;
-    console.log("Change Successful!");
-  }
-  if (valueBeingChanged === "tasks") {
-    getUserData(editedUserId).tasks = newValue;
-    console.log("Change Successful!");
-  }
-  if (valueBeingChanged === "comments") {
-    getUserData(editedUserId).comments = newValue;
-    console.log("Change Successful!");
-  }
-}
-
 function isValidId(id) {
   return databaseImplementationOrganizations.isValidId(id);
-}
-
-function deleteUser(id) {
-  databaseImplementationUsers.deleteUser(id);
 }
 
 function getOrganization(organizationName) {
@@ -87,4 +29,9 @@ function getOrganization(organizationName) {
 function writeOrganizationInfo(organizationObject) {
   databaseImplementationOrganizations.writeOrganizationInfo(organizationObject);
 }
-module.exports = { login, createUserCredentials, getUserData, setUserValue, isValidId, deleteUser, getOrganization, writeOrganizationInfo };
+module.exports = {
+  login,
+  isValidId,
+  getOrganization,
+  writeOrganizationInfo,
+};
