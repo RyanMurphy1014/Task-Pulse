53cf2d8afeadb6de110c23c31e99946a1c0076f5
RyanMurphy1014 <ryan.murphy611@gmail.com>
2023-09-03 18:35:58 -0400

    Removed MongoDb and Mongoose

--- infra/db/userCredentialModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const userCredentialSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-});
-
-module.exports = mongoose.model("userCredentials", userCredentialSchema);
